1658d7ee2912e115b8462f30bc19a6f9d453d36f
Junnel Teves <netkiko@gmail.com>
2022-03-19 17:45:49 +0800

    installed and configured less and antd

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+const withSass = require('@zeit/next-sass');
+const withLess = require('@zeit/next-less');
+const withCSS = require('@zeit/next-css');
+
+const isProd = process.env.NODE_ENV === 'production';
+
+// fix: prevents error when .less files are required by node
+if (typeof require !== 'undefined') {
+    require.extensions['.less'] = (file) => {};
 }
 
-module.exports = nextConfig
+module.exports = withCSS({
+    cssModules: true,
+    cssLoaderOptions: {
+        importLoaders: 1,
+        localIdentName: '[local]___[hash:base64:5]',
+    },
+    ...withLess(
+        withSass({
+            lessLoaderOptions: {
+                javascriptEnabled: true,
+            },
+        }),
+    ),
+});
